3d8b67094c5346e380813fde27f4e540a61658b4
Ethan Swain <eswain@uvic.ca>
2026-01-06 14:54:26 -0800

    Initial Commit

---
+++ MexicanTrainHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MexicanTrainHelper
+//
+//  Created by Ethan Swain on 2026-01-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MexicanTrainHelper/MexicanTrainHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MexicanTrainHelperApp.swift
+//  MexicanTrainHelper
+//
+//  Created by Ethan Swain on 2026-01-06.
+//
+
+import SwiftUI
+
+@main
+struct MexicanTrainHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
